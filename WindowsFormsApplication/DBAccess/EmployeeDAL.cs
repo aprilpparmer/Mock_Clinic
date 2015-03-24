f15c9a61c5b4fc88b881972347ed14ad5b5258c1
Kit Christin Nunnally <cbagwel1@my.westga.edu>
2015-03-24 19:10:51 -0400

    updated login to pass employee object

--- WindowsFormsApplication/DBAccess/EmployeeDAL.cs
+++ WindowsFormsApplication/DBAccess/EmployeeDAL.cs
@@ -10,9 +10,10 @@ namespace WindowsFormsApplication.DBAccess
     {
         public static Employee ValidNurseLogIn(String userName, String password)
         {
-            string selectStatement =
-                "Select first_name, last_name, login, password, positionID FROM employees where login = '" +
-                userName + "' and password = '" + password + "' and positionID = 1 ";
+            Employee employee = new Employee();
+
+            String selectStatement = "Select employeeID, enabled, first_name, last_name, last_login, password, positionID, login, password from employees where login = '" + userName
+              + "' and password = '" + password + "'";
 
             try
             {
@@ -26,25 +27,14 @@ namespace WindowsFormsApplication.DBAccess
                         {
                             while (reader.Read())
                             {
-                               Employee employee = new Employee();
-
-                                employee.Address = reader["address"].ToString().Trim();
-                                employee.City = reader["city"].ToString().Trim();
-                                employee.Dob = (DateTime)reader["dob"];
                                 employee.EmployeeId = (Int32)reader["employeeID"];
                                 employee.Enabled = (Byte)reader["enabled"];
                                 employee.FirstName = reader["first_name"].ToString().Trim();
-                                employee.Gender = reader["gender"].ToString().Trim();
                                 employee.LastLogin = reader["last_login"].ToString().Trim();
                                 employee.LastName = reader["last_name"].ToString().Trim();
                                 employee.Login = reader["login"].ToString().Trim();
-                                employee.MiddleInitial = reader["middle_initial"].ToString().Trim();
                                 employee.Password = reader["password"].ToString().Trim();
-                                employee.Phone = reader["phone"].ToString().Trim();
                                 employee.PositionId = (Int32)reader["positionID"];
-                                employee.Ssn = (Int32)reader["ssn"];
-                                employee.State = reader["state"].ToString().Trim();
-                                employee.Zip = (Int32)reader["zip"];
                                 return employee;
                             }
                         }
@@ -61,7 +51,7 @@ namespace WindowsFormsApplication.DBAccess
                 throw ex;
             }
 
-            return null;
+            return employee;
         }
 
         public static List<Employee> GetAllEmployees()
@@ -119,69 +109,7 @@ namespace WindowsFormsApplication.DBAccess
 
             return employeeList;
         }
-
-
-        public static Employee LogIn(String userName, String password)
-        {
-            String name = " ";
-            string selectStatement =
-                "Select * FROM employees where login = '" +
-                userName + "' and password = '" + password +"'";
-            try
-            {
-                using (SqlConnection connection = NorthwindDbConnection.GetConnection())
-                {
-                    
-                    connection.Open();
-
-                    using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
-                    {
-                        
-                        using (SqlDataReader reader = selectCommand.ExecuteReader())
-                        {
-                            
-                            while (reader.Read())
-                            {
-                                Employee employee = new Employee();
-                                employee.Address = reader["address"].ToString().Trim();
-                                employee.City = reader["city"].ToString().Trim();
-                                employee.Dob = (DateTime)reader["dob"];
-                                employee.EmployeeId = (Int32)reader["employeeID"];
-                                employee.Enabled = (Byte)reader["enabled"];
-                                employee.FirstName = reader["first_name"].ToString().Trim();
-                                employee.Gender = reader["gender"].ToString().Trim();
-                                employee.LastLogin = reader["last_login"].ToString().Trim();
-                                MessageBox.Show(@"step 5", "here");
-                                employee.LastName = reader["last_name"].ToString().Trim();
-                                employee.Login = reader["login"].ToString().Trim();
-                                MessageBox.Show(@"step 6", "here");
-                                employee.MiddleInitial = reader["middle_initial"].ToString().Trim();
-                                employee.Password = reader["password"].ToString().Trim();
-                                employee.Phone = reader["phone"].ToString().Trim();
-                                employee.PositionId = (Int32)reader["positionID"];
-                                employee.Ssn = (Int32)reader["ssn"];
-                                employee.State = reader["state"].ToString().Trim();
-                                employee.Zip = (Int32)reader["zip"];
-                                return employee;
-                            }
-                            }
-                        }
-                            
-                    }
-                }
-            
-            catch (SqlException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            
-            return null;
-
-        }
-    }          
+    }
+      
           
 }

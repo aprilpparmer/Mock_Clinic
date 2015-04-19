aa3343f5382d5038e61fe25a1be4f3025a1e9750
Cornel Stoia <cstoia1@my.westga.edu>
2015-04-19 00:38:06 -0400

    Added Allergy model class

---
+++ Application/Model/Allergy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication.Model
+{
+    class Allergy
+    {
+        public int AllergyID { get; set; }
+        public string AllergyName { get; set; }
+        public Byte Enabled { get; set; }
+    }
+}
